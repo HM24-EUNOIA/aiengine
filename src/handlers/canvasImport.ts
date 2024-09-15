108ed3ea88a56e178cbe6a1d43b1f0add7cea046
Mireya Isabel Rodríguez Pasillas <133801089+mirp01@users.noreply.github.com>
2024-09-14 23:41:55 -0600

    add json readability functions

--- src/handlers/canvasImport.ts
+++ src/handlers/canvasImport.ts
@@ -1,5 +1,14 @@
+const courseId = '492384';
+const url = `https://experiencia21.tec.mx/api/v1/courses/${courseId}/assignments`;
+
 export default {
     async fetch(request, env, ctx): Promise<Response> {
-        return new Response("bolas");
+        return fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${env.CANVAS_API_KEY}`,
+                'Content-Type': 'application/json'
+              }
+        });
+        
     }
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>; 
